2066fc486c39b8c7adf4999f6f0dc8c19ab3ddf7
Stéphane Lemay <interordi@interordi.com>
2022-06-01 22:16:12 -0400

    Remove unused imports

--- src/main/java/com/interordi/iorace/DeathListener.java
+++ src/main/java/com/interordi/iorace/DeathListener.java
@@ -6,12 +6,10 @@ import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
